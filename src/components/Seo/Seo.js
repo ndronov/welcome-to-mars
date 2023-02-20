c88773bee7e12340d1f6631a8969e2d0574d40d3
Nikita Dronov <26296037+ndronov@users.noreply.github.com>
2023-02-20 09:32:53 +0700

    add meta tags for social media

--- src/components/Seo/Seo.js
+++ src/components/Seo/Seo.js
@@ -5,7 +5,24 @@ export function Seo(props) {
 
   return (
     <>
+      <meta property="og:locale" content="en_US" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Welcome to Mars" />
+      <meta
+        property="og:description"
+        content="Mars teleport authentication form"
+      />
+      <meta
+        property="og:image"
+        content="https://www.nasa.gov/sites/default/files/styles/image_card_4x3_ratio/public/thumbnails/image/pia20282-full.jpg"
+      />
+      <meta
+        property="og:url"
+        content="https://ndronov.github.io/welcome-to-mars/"
+      />
+
       <title>{title}</title>
+
       {children}
     </>
   );
